21f15f87dd5de482f0a83b50fbd9d7aa84ca29e7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-08-25 14:09:52 +0200

    ci: update lockfile

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Script to make a deployment
 echo Starting deployment.
-git checkout 3.0/master
 git pull
 git status
-git add .
+git checkout 3.0/master
 composer update
+git add .
 git commit -m "ci: update lockfile"
 git push origin
